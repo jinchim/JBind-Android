da3437c61ef02744ab49ab0dd3b1ba146b8b1dd3
jinchim <jinchim@vip.qq.com>
2017-08-25 16:57:36 +0800

    控件类型检查

--- app/src/main/java/com/jinchim/jbind/MainActivity.java
+++ app/src/main/java/com/jinchim/jbind/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         unbinder = JBindSDK.bind(this);
 
         textView.setText("activity1");
